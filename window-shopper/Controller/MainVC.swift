54ad28c61ed4b6e6afe092d175139defafa49fef
김영석 <shinerdes@naver.com>
2018-09-03 14:52:42 +0900

    Input accessory views & IBDesignable

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by 김영석 on 2018. 9. 2..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+ 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9903632614, green: 0.5745357111, blue: 0.01274232651, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+    
+    @objc func calculate() {
+        print("We got here")
+    
+    }
+
+
+}
+
---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,42 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by 김영석 on 2018. 9. 2..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+      
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        
+        if let p = placeholder {
+            
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
